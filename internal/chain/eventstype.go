3d8b7c40b9ad8be232d1cf7ef3bd14d19fb1e28a
ted-tech <ted@cess.one>
2022-05-25 23:39:46 +0800

    Fix the problem of increasing pledge and quit mining, and fix the function of clearing invalid files.

--- internal/chain/eventstype.go
+++ internal/chain/eventstype.go
@@ -195,6 +195,12 @@ type Event_RecoverFile struct {
 	Topics []types.Hash
 }
 
+type Event_ReceiveSpace struct {
+	Phase  types.Phase
+	Acc    types.AccountID
+	Topics []types.Hash
+}
+
 //------------------------FileMap--------------------------------
 type Event_RegistrationScheduler struct {
 	Phase  types.Phase
@@ -267,6 +273,7 @@ type MyEventRecords struct {
 	FileBank_FillerUpload         []Event_FillerUpload
 	FileBank_ClearInvalidFile     []Event_ClearInvalidFile
 	FileBank_RecoverFile          []Event_RecoverFile
+	FileBank_ReceiveSpace         []Event_ReceiveSpace
 	//FileMap
 	FileMap_RegistrationScheduler []Event_RegistrationScheduler
 	//other system
