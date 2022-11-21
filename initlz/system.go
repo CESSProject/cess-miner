420ff22655bc432065de525016f09f987b925b95
AstaFrode <frode@cess.one>
2022-11-21 17:00:32 +0800

    Increase the function of clearing the memory cache and setting the number of cpu 2/3 cores

--- initlz/system.go
+++ initlz/system.go
@@ -12,7 +12,7 @@ func SystemInit() {
 		fmt.Printf("\x1b[%dm[err]\x1b[0m Please execute on Linux system\n", 41)
 		os.Exit(1)
 	}
-	setAllCores()
+	setCores()
 }
 
 func runOnLinuxSystem() bool {
@@ -23,6 +23,13 @@ func runWithRootPrivileges() bool {
 	return os.Geteuid() == 0
 }
 
-func setAllCores() {
-	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+// Allocate 2/3 cores to the program
+func setCores() {
+	num := runtime.NumCPU()
+	num = num * 2 / 3
+	if num <= 1 {
+		runtime.GOMAXPROCS(1)
+	} else {
+		runtime.GOMAXPROCS(num)
+	}
 }
