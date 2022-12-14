5431292ef8dce6ad7ebee8b4a2f691f37ad2aad9
AstaFrode <frode@cess.one>
2022-12-14 17:30:52 +0800

    Upgrade the logger module

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,87 +1,138 @@
+/*
+   Copyright 2022 CESS (Cumulus Encrypted Storage System) authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package logger
 
 import (
-	"fmt"
-	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 	"time"
 
-	"github.com/CESSProject/cess-bucket/configs"
-
 	"github.com/natefinch/lumberjack"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var (
-	Out *zap.Logger
-	Err *zap.Logger
-	Uld *zap.Logger
-	Dld *zap.Logger
-	Flr *zap.Logger
-	Pnc *zap.Logger
-	Chg *zap.Logger
-	Del *zap.Logger
-)
-
-func LoggerInit() {
-	_, err := os.Stat(configs.LogfileDir)
-	if err != nil {
-		err = os.MkdirAll(configs.LogfileDir, os.ModeDir)
-		if err != nil {
-			fmt.Printf("\x1b[%dm[err]\x1b[0m %v\n", 41, err)
-			os.Exit(1)
-		}
-	}
-
-	var log_file = []string{
-		"common.log",
-		"error.log",
-		"upfile.log",
-		"downfile.log",
-		"filler.log",
-		"panic.log",
-		"challenge.log",
-		"deleted.log",
-	}
-
-	for i := 0; i < len(log_file); i++ {
-		Encoder := GetEncoder()
-		fpath := filepath.Join(configs.LogfileDir, log_file[i])
-		WriteSyncer := GetWriteSyncer(fpath)
-		newCore := zapcore.NewTee(zapcore.NewCore(Encoder, WriteSyncer, zap.NewAtomicLevel()))
-		switch i {
-		case 0:
-			Out = zap.New(newCore, zap.AddCaller())
-			Out.Sugar().Infof("%v", fpath)
-		case 1:
-			Err = zap.New(newCore, zap.AddCaller())
-			Err.Sugar().Infof("%v", fpath)
-		case 2:
-			Uld = zap.New(newCore, zap.AddCaller())
-			Uld.Sugar().Infof("%v", fpath)
-		case 3:
-			Dld = zap.New(newCore, zap.AddCaller())
-			Dld.Sugar().Infof("%v", fpath)
-		case 4:
-			Flr = zap.New(newCore, zap.AddCaller())
-			Flr.Sugar().Infof("%v", fpath)
-		case 5:
-			Pnc = zap.New(newCore, zap.AddCaller())
-			Pnc.Sugar().Infof("%v", fpath)
-		case 6:
-			Chg = zap.New(newCore, zap.AddCaller())
-			Chg.Sugar().Infof("%v", fpath)
-		case 7:
-			Del = zap.New(newCore, zap.AddCaller())
-			Del.Sugar().Infof("%v", fpath)
-		}
-	}
-	log.Println("Log loading succeeded")
+type Logger interface {
+	Log(string, string, error)
+	Pnc(string, error)
+	Common(string, error)
+	Upfile(string, error)
 }
 
-func GetEncoder() zapcore.Encoder {
+type logs struct {
+	logpath map[string]string
+	log     map[string]*zap.Logger
+}
+
+func NewLogs(logfiles map[string]string) (Logger, error) {
+	var (
+		logpath = make(map[string]string, 0)
+		logCli  = make(map[string]*zap.Logger)
+	)
+	for name, fpath := range logfiles {
+		dir := getFilePath(fpath)
+		_, err := os.Stat(dir)
+		if err != nil {
+			err = os.MkdirAll(dir, os.ModeDir)
+			if err != nil {
+				return nil, errors.Errorf("%v,%v", dir, err)
+			}
+		}
+		Encoder := getEncoder()
+		newCore := zapcore.NewTee(
+			zapcore.NewCore(Encoder, getWriteSyncer(fpath), zap.NewAtomicLevel()),
+		)
+		logpath[name] = fpath
+		logCli[name] = zap.New(newCore, zap.AddCaller())
+		logCli[name].Sugar().Infof("%v", fpath)
+	}
+	return &logs{
+		logpath: logpath,
+		log:     logCli,
+	}, nil
+}
+
+func (l *logs) Log(name, level string, err error) {
+	_, file, line, _ := runtime.Caller(1)
+	v, ok := l.log[name]
+	if ok {
+		switch level {
+		case "info":
+			v.Sugar().Infof("[%v:%d] %v", filepath.Base(file), line, err)
+		case "error", "err":
+			v.Sugar().Errorf("[%v:%d] %v", filepath.Base(file), line, err)
+		case "warn":
+			v.Sugar().Warnf("[%v:%d] %v", filepath.Base(file), line, err)
+		}
+	}
+}
+
+func (l *logs) Pnc(level string, err error) {
+	_, file, line, _ := runtime.Caller(1)
+	v, ok := l.log["panic"]
+	if ok {
+		switch level {
+		case "error", "err":
+			v.Sugar().Errorf("[%v:%d] %v", filepath.Base(file), line, err)
+		}
+	}
+}
+
+func (l *logs) Common(level string, err error) {
+	_, file, line, _ := runtime.Caller(1)
+	v, ok := l.log["common"]
+	if ok {
+		switch level {
+		case "info":
+			v.Sugar().Infof("[%v:%d] %v", filepath.Base(file), line, err)
+		case "error", "err":
+			v.Sugar().Errorf("[%v:%d] %v", filepath.Base(file), line, err)
+		case "warn":
+			v.Sugar().Warnf("[%v:%d] %v", filepath.Base(file), line, err)
+		}
+	}
+}
+
+func (l *logs) Upfile(level string, err error) {
+	_, file, line, _ := runtime.Caller(1)
+	v, ok := l.log["upfile"]
+	if ok {
+		switch level {
+		case "info":
+			v.Sugar().Infof("[%v:%d] %v", filepath.Base(file), line, err)
+		case "error", "err":
+			v.Sugar().Errorf("[%v:%d] %v", filepath.Base(file), line, err)
+		case "warn":
+			v.Sugar().Warnf("[%v:%d] %v", filepath.Base(file), line, err)
+		}
+	}
+}
+
+func getFilePath(fpath string) string {
+	path, _ := filepath.Abs(fpath)
+	index := strings.LastIndex(path, string(os.PathSeparator))
+	ret := path[:index]
+	return ret
+}
+
+func getEncoder() zapcore.Encoder {
 	return zapcore.NewConsoleEncoder(
 		zapcore.EncoderConfig{
 			TimeKey:        "ts",
@@ -95,14 +146,14 @@ func GetEncoder() zapcore.Encoder {
 			EncodeLevel:    cEncodeLevel,
 			EncodeTime:     cEncodeTime,
 			EncodeDuration: zapcore.SecondsDurationEncoder,
-			EncodeCaller:   cEncodeCaller,
+			EncodeCaller:   nil,
 		})
 }
 
-func GetWriteSyncer(fpath string) zapcore.WriteSyncer {
+func getWriteSyncer(fpath string) zapcore.WriteSyncer {
 	lumberJackLogger := &lumberjack.Logger{
 		Filename:   fpath,
-		MaxSize:    30,
+		MaxSize:    10,
 		MaxBackups: 99,
 		MaxAge:     180,
 		LocalTime:  true,
@@ -117,8 +168,4 @@ func cEncodeLevel(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
 
 func cEncodeTime(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString("[" + t.Format("2006-01-02 15:04:05") + "]")
-}
-
-func cEncodeCaller(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
-	enc.AppendString("[" + caller.TrimmedPath() + "]")
 }
