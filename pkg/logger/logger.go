69a7dceaedec7d740396031dbe0b276e093b7ba9
AstaFrode <frode@cess.one>
2023-04-07 16:43:31 +0800

    update logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,87 +1,132 @@
+/*
+	Copyright (C) CESS. All rights reserved.
+	Copyright (C) Cumulus Encrypted Storage System. All rights reserved.
+
+	SPDX-License-Identifier: Apache-2.0
+*/
+
 package logger
 
 import (
-	"fmt"
-	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 	"time"
 
 	"github.com/CESSProject/cess-bucket/configs"
-
 	"github.com/natefinch/lumberjack"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var (
-	Out *zap.Logger
-	Err *zap.Logger
-	Uld *zap.Logger
-	Dld *zap.Logger
-	Flr *zap.Logger
-	Pnc *zap.Logger
-	Chg *zap.Logger
-	Del *zap.Logger
-)
-
-func LoggerInit() {
-	_, err := os.Stat(configs.LogfileDir)
-	if err != nil {
-		err = os.MkdirAll(configs.LogfileDir, os.ModeDir)
-		if err != nil {
-			fmt.Printf("\x1b[%dm[err]\x1b[0m %v\n", 41, err)
-			os.Exit(1)
-		}
-	}
-
-	var log_file = []string{
-		"common.log",
-		"error.log",
-		"upfile.log",
-		"downfile.log",
-		"filler.log",
-		"panic.log",
-		"challenge.log",
-		"deleted.log",
-	}
-
-	for i := 0; i < len(log_file); i++ {
-		Encoder := GetEncoder()
-		fpath := filepath.Join(configs.LogfileDir, log_file[i])
-		WriteSyncer := GetWriteSyncer(fpath)
-		newCore := zapcore.NewTee(zapcore.NewCore(Encoder, WriteSyncer, zap.NewAtomicLevel()))
-		switch i {
-		case 0:
-			Out = zap.New(newCore, zap.AddCaller())
-			Out.Sugar().Infof("%v", fpath)
-		case 1:
-			Err = zap.New(newCore, zap.AddCaller())
-			Err.Sugar().Infof("%v", fpath)
-		case 2:
-			Uld = zap.New(newCore, zap.AddCaller())
-			Uld.Sugar().Infof("%v", fpath)
-		case 3:
-			Dld = zap.New(newCore, zap.AddCaller())
-			Dld.Sugar().Infof("%v", fpath)
-		case 4:
-			Flr = zap.New(newCore, zap.AddCaller())
-			Flr.Sugar().Infof("%v", fpath)
-		case 5:
-			Pnc = zap.New(newCore, zap.AddCaller())
-			Pnc.Sugar().Infof("%v", fpath)
-		case 6:
-			Chg = zap.New(newCore, zap.AddCaller())
-			Chg.Sugar().Infof("%v", fpath)
-		case 7:
-			Del = zap.New(newCore, zap.AddCaller())
-			Del.Sugar().Infof("%v", fpath)
-		}
-	}
-	log.Println("Log loading succeeded")
+type Logger interface {
+	Log(level string, msg string)
+	Pnc(level string, err error)
+	Upfile(level string, msg string)
+	Downfile(level string, msg string)
 }
 
-func GetEncoder() zapcore.Encoder {
+type logs struct {
+	logpath map[string]string
+	log     map[string]*zap.Logger
+}
+
+func NewLogs(logfiles map[string]string) (Logger, error) {
+	var (
+		logpath = make(map[string]string, 0)
+		logCli  = make(map[string]*zap.Logger)
+	)
+	for name, fpath := range logfiles {
+		dir := getFilePath(fpath)
+		_, err := os.Stat(dir)
+		if err != nil {
+			err = os.MkdirAll(dir, configs.DirPermission)
+			if err != nil {
+				return nil, errors.Errorf("%v,%v", dir, err)
+			}
+		}
+		Encoder := getEncoder()
+		newCore := zapcore.NewTee(
+			zapcore.NewCore(Encoder, getWriteSyncer(fpath), zap.NewAtomicLevel()),
+		)
+		logpath[name] = fpath
+		logCli[name] = zap.New(newCore, zap.AddCaller())
+		logCli[name].Sugar().Infof("%v", fpath)
+	}
+	return &logs{
+		logpath: logpath,
+		log:     logCli,
+	}, nil
+}
+
+func (l *logs) Log(level string, msg string) {
+	_, file, line, _ := runtime.Caller(1)
+	v, ok := l.log["log"]
+	if ok {
+		switch level {
+		case "info":
+			v.Sugar().Infof("[%v:%d] %s", filepath.Base(file), line, msg)
+		case "err":
+			v.Sugar().Errorf("[%v:%d] %s", filepath.Base(file), line, msg)
+		}
+	}
+}
+
+func (l *logs) Pnc(level string, err error) {
+	_, file, line, _ := runtime.Caller(1)
+	v, ok := l.log["panic"]
+	if ok {
+		switch level {
+		case "err":
+			v.Sugar().Errorf("[%v:%d] %v", filepath.Base(file), line, err)
+		}
+	}
+}
+
+func (l *logs) Upfile(level string, msg string) {
+	_, file, line, _ := runtime.Caller(1)
+	v, ok := l.log["upfile"]
+	if ok {
+		switch level {
+		case "info":
+			v.Sugar().Infof("[%v:%d] %s", filepath.Base(file), line, msg)
+		case "err":
+			v.Sugar().Errorf("[%v:%d] %s", filepath.Base(file), line, msg)
+		}
+	}
+}
+
+func (l *logs) Downfile(level string, msg string) {
+	_, file, line, _ := runtime.Caller(1)
+	v, ok := l.log["downfile"]
+	if ok {
+		switch level {
+		case "info":
+			v.Sugar().Infof("[%v:%d] %v", filepath.Base(file), line, msg)
+		case "err":
+			v.Sugar().Errorf("[%v:%d] %v", filepath.Base(file), line, msg)
+		}
+	}
+}
+
+func (l *logs) Record(err error) {
+	_, file, line, _ := runtime.Caller(1)
+	v, ok := l.log["record"]
+	if ok {
+		v.Sugar().Infof("[%v:%d] %v", filepath.Base(file), line, err)
+	}
+}
+
+func getFilePath(fpath string) string {
+	path, _ := filepath.Abs(fpath)
+	index := strings.LastIndex(path, string(os.PathSeparator))
+	ret := path[:index]
+	return ret
+}
+
+func getEncoder() zapcore.Encoder {
 	return zapcore.NewConsoleEncoder(
 		zapcore.EncoderConfig{
 			TimeKey:        "ts",
@@ -95,14 +140,14 @@ func GetEncoder() zapcore.Encoder {
 			EncodeLevel:    cEncodeLevel,
 			EncodeTime:     cEncodeTime,
 			EncodeDuration: zapcore.SecondsDurationEncoder,
-			EncodeCaller:   cEncodeCaller,
+			EncodeCaller:   nil,
 		})
 }
 
-func GetWriteSyncer(fpath string) zapcore.WriteSyncer {
+func getWriteSyncer(fpath string) zapcore.WriteSyncer {
 	lumberJackLogger := &lumberjack.Logger{
 		Filename:   fpath,
-		MaxSize:    30,
+		MaxSize:    10,
 		MaxBackups: 99,
 		MaxAge:     180,
 		LocalTime:  true,
@@ -117,8 +162,4 @@ func cEncodeLevel(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
 
 func cEncodeTime(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString("[" + t.Format("2006-01-02 15:04:05") + "]")
-}
-
-func cEncodeCaller(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
-	enc.AppendString("[" + caller.TrimmedPath() + "]")
 }
---
+++ pkg/logger/logger_test.go
@@ -0,0 +1,22 @@
+/*
+	Copyright (C) CESS. All rights reserved.
+	Copyright (C) Cumulus Encrypted Storage System. All rights reserved.
+
+	SPDX-License-Identifier: Apache-2.0
+*/
+
+package logger
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewLogs(t *testing.T) {
+	log_files := make(map[string]string, 2)
+	log_files["info"] = "./info.log"
+	log_files["err"] = "./err.log"
+	_, err := NewLogs(log_files)
+	assert.NoError(t, err)
+}
