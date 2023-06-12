fddd9e8ed7e5f24a908106289160dd3c1b987486
AstaFrode <frode@cess.one>
2023-06-12 17:01:26 +0800

    update p2p to v0.0.33 and runcmd (#104)

--- cmd/console/runCmd.go
+++ cmd/console/runCmd.go
@@ -88,7 +88,7 @@ func runCmd(cmd *cobra.Command, args []string) {
 		context.Background(),
 		p2pgo.ListenPort(n.GetServicePort()),
 		p2pgo.Workspace(filepath.Join(n.GetWorkspace(), n.GetStakingAcc(), n.GetRoleName())),
-		p2pgo.BootPeers(boots),
+		p2pgo.BootPeers(bootstrap),
 	)
 	if err != nil {
 		configs.Err(fmt.Sprintf("[p2pgo.New] %v", err))
