fd0bd51122af9cb53962140770f82f5a433c1eac
AstaFrode <frode@cess.one>
2023-06-14 23:39:14 +0800

    update chainMgt

--- node/chainMgt.go
+++ node/chainMgt.go
@@ -32,26 +32,27 @@ func (n *Node) chainMgt(ch chan bool) {
 	var ok bool
 	var err error
 	var peerid string
-	var addr string
-	var multiaddr string
+	var maAddr ma.Multiaddr
+	var addrInfo *peer.AddrInfo
 	var boots []string
 	var teeList []pattern.TeeWorkerInfo
 	var deossList []string
+	var bootstrap []string
 	var lastMem uint64
 	tickListening := time.NewTicker(time.Second * 30)
 	defer tickListening.Stop()
 
 	memSt := &runtime.MemStats{}
-	tikProgram := time.NewTicker(time.Second * 3)
+	tikProgram := time.NewTicker(time.Second * 5)
 	defer tikProgram.Stop()
 
-	n.Log("info", ">>>>> Start log")
+	n.Log("info", ">>>>> Start chainMgt")
 
 	for {
 		select {
 		case <-tikProgram.C:
 			runtime.ReadMemStats(memSt)
-			if memSt.HeapSys >= pattern.SIZE_1GiB*4 {
+			if memSt.HeapSys >= pattern.SIZE_1GiB*2 {
 				if memSt.HeapAlloc != lastMem {
 					n.Log("err", fmt.Sprintf("memory usage: %d bytes", memSt.HeapAlloc))
 				}
@@ -64,22 +65,32 @@ func (n *Node) chainMgt(ch chan bool) {
 				n.SetChainState(false)
 				err = n.Reconnect()
 				if err != nil {
+					n.Log("err", pattern.ERR_RPC_CONNECTION.Error())
 					configs.Err(pattern.ERR_RPC_CONNECTION.Error())
 				}
 			}
 			boots = n.GetBootNodes()
 			for _, b := range boots {
-				bootstrap, _ := sutils.ParseMultiaddrs(b)
+				bootstrap, err = sutils.ParseMultiaddrs(b)
+				if err != nil {
+					n.Log("err", fmt.Sprintf("[ParseMultiaddrs %v] %v", b, err))
+					continue
+				}
 				for _, v := range bootstrap {
-					addr, err := ma.NewMultiaddr(v)
+					maAddr, err = ma.NewMultiaddr(v)
 					if err != nil {
 						continue
 					}
-					addrInfo, err := peer.AddrInfoFromP2pAddr(addr)
+					addrInfo, err = peer.AddrInfoFromP2pAddr(maAddr)
 					if err != nil {
 						continue
 					}
-					n.SaveAndUpdateTeePeer(addrInfo.ID.Pretty(), 0)
+					err = n.Connect(n.GetRootCtx(), *addrInfo)
+					if err != nil {
+						n.Log("err", err.Error())
+						continue
+					}
+					n.SaveTeePeer(addrInfo.ID.Pretty(), 0)
 				}
 			}
 			if !n.GetDiscoverSt() {
@@ -87,38 +98,37 @@ func (n *Node) chainMgt(ch chan bool) {
 			}
 		case discoverPeer := <-n.DiscoveredPeer():
 			peerid = discoverPeer.ID.Pretty()
-			err := n.Connect(n.GetRootCtx(), discoverPeer)
+			err = n.Connect(n.GetRootCtx(), discoverPeer)
 			if err == nil {
 				n.Log("info", fmt.Sprintf("discover and connect to %s", peerid))
 			}
 
 			for _, v := range discoverPeer.Addrs {
-				addr = v.String()
-				temp := strings.Split(addr, "/")
-				for _, vv := range temp {
+				boots = strings.Split(v.String(), "/")
+				for _, vv := range boots {
 					if sutils.IsIPv4(vv) {
 						if vv[len(vv)-1] == byte(49) && vv[len(vv)-3] == byte(48) {
 							continue
 						}
-						multiaddr = fmt.Sprintf("%s/p2p/%s", addr, peerid)
-						n.AddMultiaddrToPeerstore(multiaddr, time.Hour)
+						n.AddMultiaddrToPeerstore(fmt.Sprintf("%s/p2p/%s", v.String(), peerid), time.Hour)
 						break
 					}
 				}
 			}
 
 			if n.HasStoragePeer(peerid) {
-				continue
+				break
 			}
 			if n.HasDeossPeer(peerid) {
-				continue
+				break
 			}
 			if n.HasTeePeer(peerid) {
-				continue
+				break
 			}
+
 			teeList, err = n.QueryTeeInfoList()
 			if err != nil {
-				continue
+				break
 			}
 			for _, v := range teeList {
 				if peerid == base58.Encode([]byte(string(v.PeerId[:]))) {
@@ -128,12 +138,12 @@ func (n *Node) chainMgt(ch chan bool) {
 				}
 			}
 			if n.HasTeePeer(peerid) {
-				continue
+				break
 			}
 
 			deossList, err = n.QueryDeossPeerIdList()
 			if err != nil {
-				continue
+				break
 			}
 
 			for _, v := range deossList {
@@ -145,11 +155,12 @@ func (n *Node) chainMgt(ch chan bool) {
 			}
 
 			if n.HasDeossPeer(peerid) {
-				continue
+				break
 			}
 
 			n.SaveStoragePeer(peerid, "")
 			configs.Tip(fmt.Sprintf("discovered a storage node: %s", peerid))
 		}
+		time.Sleep(time.Millisecond * 10)
 	}
 }
