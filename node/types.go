56cf6cb7848ecea7a288282d66132a55dfb92518
AstaFrode <frode@cess.one>
2023-06-23 23:58:46 +0800

    update challenge

--- node/types.go
+++ node/types.go
@@ -17,18 +17,20 @@ const (
 )
 
 const (
-	Cach_prefix_metadata      = "metadata:"
-	Cach_prefix_report        = "report:"
-	Cach_prefix_idle          = "idle:"
-	Cach_prefix_idleSiama     = "sigmaidle:"
-	Cach_prefix_serviceSiama  = "sigmaservice:"
-	Cach_AggrProof_Reported   = "AggrProof_Reported"
-	Cach_AggrProof_Transfered = "AggrProof_Transfered"
-	Cach_prefix_MyLost        = "mylost:"
-	Cach_prefix_recovery      = "recovery:"
-	Cach_prefix_TargetMiner   = "targetminer:"
-	Cach_prefix_File          = "file:"
-	Cach_prefix_ParseBlock    = "parseblocks"
+	Cach_prefix_metadata       = "metadata:"
+	Cach_prefix_report         = "report:"
+	Cach_prefix_idle           = "idle:"
+	Cach_prefix_idleSiama      = "sigmaidle:"
+	Cach_prefix_serviceSiama   = "sigmaservice:"
+	Cach_AggrProof_Reported    = "AggrProofReported"
+	Cach_AggrProof_Transfered  = "AggrProofTransfered"
+	Cach_IdleChallengeBlock    = "ChallengeIdleBlock"
+	Cach_ServiceChallengeBlock = "ChallengeServiceBlock"
+	Cach_prefix_MyLost         = "mylost:"
+	Cach_prefix_recovery       = "recovery:"
+	Cach_prefix_TargetMiner    = "targetminer:"
+	Cach_prefix_File           = "file:"
+	Cach_prefix_ParseBlock     = "parseblocks"
 )
 
 const P2PResponseOK uint32 = 200
