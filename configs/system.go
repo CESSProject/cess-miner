da6110f7638f249c2079ffe955e6b2de786b2c90
AstaFrode <frode@cess.one>
2024-09-20 14:35:29 +0800

    Update version to v0.7.16

--- configs/system.go
+++ configs/system.go
@@ -22,7 +22,7 @@ const (
 	// Name is the name of the program
 	Name = "miner"
 	// version
-	Version = "v0.7.15"
+	Version = "v0.7.16"
 	// Description is the description of the program
 	Description = "Storage miner implementation in CESS networks"
 	// NameSpace is the cached namespace
